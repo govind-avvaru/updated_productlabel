9d856da1688102a29f55c7c4c803b8f64a23e733
govind-avvaru <98160328+govind-avvaru@users.noreply.github.com>
2023-01-06 10:35:25 +0530

    Update Collection.php

--- I95Dev/Productlabel/Model/ResourceModel/ProductLabel/Grid/Collection.php
+++ I95Dev/Productlabel/Model/ResourceModel/ProductLabel/Grid/Collection.php
@@ -37,17 +37,25 @@ class Collection extends \Smile\ProductLabel\Model\ResourceModel\ProductLabel\Gr
      */
     protected function _renderFiltersBefore()
     {
-        $this->getSelect()->joinInner(
+       $this->getSelect()->joinInner(
             ['ea' => $this->getTable('eav_attribute')],
             'ea.attribute_id = main_table.attribute_id',
             ['frontend_label']
         );
-
         $this->getSelect()->joinLeft(
+            ['cae'=>$this->getTable('catalog_category_entity')],
+            'cae.entity_id = main_table.category_id',
+            ['row_id']);
+      $this->getSelect()->joinLeft(
+            ['ca' => $this->getTable('catalog_category_entity_varchar')],
+            'ca.row_id=cae.row_id',
+            ['value AS category']
+        );
+     $this->getSelect()->joinLeft(
             ['eaov' => $this->getTable('eav_attribute_option_value')],
             'eaov.option_id = main_table.option_id',
             ['option_label' => 'value']
-        );
+        )->group('main_table.product_label_id');
 
     }
 }
